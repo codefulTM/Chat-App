b5dd1bf9ef4291f2de61940a5110a6b3e54fb003
codefulTM <nguyentuanminh435@gmail.com>
2025-09-25 22:16:32 +0700

    Updated auth.ts by adding logic to logic route

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -44,12 +44,30 @@ router.post('register', async (req, res) => {
     }
 });
 
-router.post('login', (req, res) => {
+router.post('login', async (req, res) => {
     try {
         // check if the information in req.body is correct
-        // if the information is incorrect -> throw error
+        // find user with matching username
+        const user = await UserModel.findOne({username: req.body.username});
+        if(!user) {
+            throw new Error('Incorrect username');
+        }
 
+        // get the user's hashed password
+        const hashedPassword = user.password;
+
+        // compare the hashed password with the password in req.body
         // if the information is correct -> return a jwt token
+        if(bcrypt.compareSync(req.body.password, hashedPassword)) {
+            const token = jwt.sign({
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                displayName: user.displayName
+            }, process.env.JWT_SECRET!);
+            return res.json(token);
+        }
+        throw new Error('Incorrect password');
     }
     catch(err) {
         console.log((err as Error).message);
