fee97c429b5d87cc23d13e8ee374cb23123d086a
codefulTM <nguyentuanminh435@gmail.com>
2025-09-25 21:51:21 +0700

    Created auth.ts and updated register function

---
+++ server/routes/auth.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import UserModel from '../models/User.js';
+
+const router = express.Router();
+
+router.post('register', async (req, res) => {
+    try {
+        if(!req.body.username && !req.body.email && !req.body.password && !req.body.displayName) {
+            throw new Error('All fields are required');
+        }
+
+        if(req.body.displayName === '') {
+            throw new Error('Display name cannot be empty');
+        }
+
+        let user = undefined;
+        // if the username already exists -> throw error
+        user = await UserModel.findOne({username: req.body.username});
+        if(user) {
+            throw new Error('Username already exists');
+        }
+
+        // if the email already exists -> throw error
+        user = await UserModel.findOne({email: req.body.email});
+        if(user) {
+            throw new Error('Email already exists');
+        }
+
+        res.json(await UserModel.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            displayName: req.body.displayName,
+            lastOnline: Date.now
+        }));
+    }
+    catch(err) {
+        console.log((err as Error).message);
+    }
+});
+
+router.post('login', (req, res) => {
+
+})
+
+export default router;
