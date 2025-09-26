9b24836916569807846ca77909a2b8b7dc5663b1
codefulTM <nguyentuanminh435@gmail.com>
2025-09-26 19:25:00 +0700

    Update conversations.ts

--- server/routes/conversations.ts
+++ server/routes/conversations.ts
@@ -1,5 +1,6 @@
 import jwtMiddleware from "../middlewares/jwtMiddleware.js";
 import express from 'express';
+import MessageModel from "../models/Message.js";
 import ConversationModel from "../models/Conversation.js";
 
 const router = express.Router();
@@ -12,6 +13,29 @@ router.get('/', jwtMiddleware, async (req, res) => {
         return res.json({
             success: true,
             message: conversations
+        });
+    }
+    catch(err) {
+        return res.json({
+            success: false,
+            message: 'Database error'
+        });
+    }
+});
+
+router.get('/:conversationId/messages', async (req, res) => {
+    const { limit = 10, skip = 0 } = req.query;
+    const conversationId = req.params.conversationId;
+    try {
+        const messages = await MessageModel.find({
+            conversationId: conversationId
+        })
+            .skip(Number(skip))
+            .limit(Number(limit))
+            .sort({createdAt: -1});
+        return res.json({
+            success: true,
+            message: messages
         });
     }
     catch(err) {
